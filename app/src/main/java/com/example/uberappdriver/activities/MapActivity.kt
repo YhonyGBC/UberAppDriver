2a0d3d56be97c84a737d569a13d414f7f672184f
jbenavides <jhonattangbc@udenar.edu.co>
2023-12-01 23:18:20 -0500

    commit google maps

--- app/src/main/java/com/example/uberappdriver/activities/MapActivity.kt
+++ app/src/main/java/com/example/uberappdriver/activities/MapActivity.kt
@@ -2,11 +2,17 @@ package com.example.uberappdriver.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.uberappdriver.R
 import com.example.uberappdriver.databinding.ActivityMapBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private lateinit var binding: ActivityMapBinding
+    private var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,16 @@ class MapActivity : AppCompatActivity() {
         binding = ActivityMapBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+    }
+
+
 }
